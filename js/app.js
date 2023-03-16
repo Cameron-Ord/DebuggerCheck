74b16692672baeb98b5ed0028c5b090ddefa4bba
cameron <hammerbro_97@hotmail.com>
2023-03-16 17:44:24 -0600

    added conditionals

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let username = "Cameron";
+let user_age = 30;
+let is_member = true;
+
+if(username == 'Cameron' || (user_age > 30 && is_member === true) ){
+
+console.log("wow you are sooooooooooooooooooo special");
+
+}else{
+
+    console.log("otherwise print out a standard greeting");
+
+}
